8d08583fcb88a713693e3d1bcb443d3ea90af93b
prisyafandiafif <prisyafandiafif.charifa@gmail.com>
2019-06-18 17:40:40 +0700

    Update chripplugin.js

--- www/chripplugin.js
+++ www/chripplugin.js
@@ -1,5 +1,15 @@
 function ChripPlugin() {
 }
+//Get Data To Send
+ChripPlugin.prototype.getDataToSend = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, 'ChripPlugin', 'getDataToSend');
+};
+
+//Get Data Received
+ChripPlugin.prototype.getDataReceived = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, 'ChripPlugin', 'getDataReceived');
+};
+
 //Send Data Plugin
 ChripPlugin.prototype.sendData = function (dataToSend, successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, 'ChripPlugin', 'sendData',[dataToSend]);
